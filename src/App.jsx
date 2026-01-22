742cc9ce02e6e4a9e15c16422e33583804173594
Kerstein Ashby San Pedro <mkasanpedro@tip.edu.ph>
2026-01-22 18:35:09 +0800

    Add UserAPI component to fetch and display users

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,17 @@
-import UserList from './UserList';
-import Counter from './Counter'; // import it
+import UserAPI from './UserAPI';
 
 function App() {
   const users = ['Alice', 'Bob', 'Charlie'];
-  const isLoggedIn = false;
+  const isLoggedIn = true;
 
   return (
     <div>
       <h1>React ES6 Lab</h1>
-
       {isLoggedIn ? <p>Welcome back!</p> : <p>Please log in</p>}
-
       <UserList users={users} />
-      <Counter /> {/* render it here */}
+      <Counter />
+      <h2>Users from API:</h2>
+      <UserAPI />
     </div>
   );
 }
